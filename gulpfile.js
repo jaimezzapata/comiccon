42f0c144be278cb74141f171455694551a94f806
Jaime Zapata <zapataval2304@gmail.com>
2022-03-01 17:04:33 -0500

    Se agrea tarea para optimizar png y jpg

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,47 @@
-const { src, dest, watch, parallel } = require('gulp');
+const { src, dest, watch, parallel } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
-const plumber = require('gulp-plumber');
-const webp = require('gulp-webp');
-const imagemin = require('gulp-imagemin');
-const cache = require('gulp-cache');
+const plumber = require("gulp-plumber");
+const webp = require("gulp-webp");
+const imagemin = require("gulp-imagemin");
+const cache = require("gulp-cache");
 
 function compilarSass(done) {
-    src('src/scss/**/*.scss')
+    src("src/scss/**/*.scss")
         .pipe(plumber())
         .pipe(sass())
-        .pipe(dest('build/css'));
+        .pipe(dest("build/css"));
     done();
-};
+}
 
-function convertirWebp(done){
-
+function convertirWebp(done) {
     const opciones = {
-        optimizationLevel: 3
+        quality: 50
     };
 
-    src('./src/img/**/*.{png,jpg}')
-    .pipe( cache( imagemin(opciones) ) )
-    .pipe( dest('build/img') )
+    src("./src/img/**/*.{png,jpg}")
+        .pipe(cache(imagemin(opciones)))
+        .pipe(dest("build/img"));
 
     done();
 }
 
-function calidadImg(done){
+function calidadImg(done) {
 
+    const opciones = {
+        optimizationLevel: 3,
+    };
 
-    src('./src/img/**/*.{png,jpg}')
-    .pipe( webp(opciones) )
-    .pipe( dest('build/img') )
+    src("./src/img/**/*.{png,jpg}")
+        .pipe(webp(opciones))
+        .pipe(dest("build/img"));
     done();
 }
 
 function dev(done) {
-    watch('src/scss/**/*.scss')
+    watch("src/scss/**/*.scss");
     done();
 }
 exports.compilarSass = compilarSass;
+exports.calidadImg = calidadImg;
 exports.convertirWebp = convertirWebp;
-exports.dev = parallel(convertirWebp, dev, compilarSass);
+exports.dev = parallel(calidadImg, convertirWebp, dev, compilarSass);
